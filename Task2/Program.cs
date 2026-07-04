fbc924f4e02cccfb637ea6f6ca59250ca7028665
HibaYaqoob <hebaalmusalami.gail.com>
2026-07-04 23:38:47 +0400

    Solving Task 1: Countdown Timer

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Task 1: Countdown Timer
+
+            Console.Write("Enter the starting number: ");
+            int startNumber = int.Parse(Console.ReadLine());
+
+            for (int i = startNumber; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+                
+            }
+            Console.WriteLine("Liftoff");
         }
     }
 }
